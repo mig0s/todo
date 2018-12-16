7d2ed1bb2af68a31c35486790b05cdb0fc567ad8
Mikhail Gospodarikov <mail@mig0s.tk>
2018-12-16 20:12:07 +0800

    Column and Card initial structure
    Added custom elements todo-column and todo-card

---
+++ resources/script.js
@@ -0,0 +1,142 @@
+/*
+ *
+ * COMPONENTS
+ *
+ */
+
+// COMMON ELEMENT CLASS
+class ToDoElement extends HTMLElement {
+    constructor() {
+        // CALL PARENT CONSTRUCTOR (HTMLElement)
+        super();
+
+        // INITIATE SHADOW ROOT AND ADD STYLES
+        this.root = this.attachShadow({ mode: "open" });
+        this.root.innerHTML = `
+        <style>
+        .cell {
+            padding: 5px;
+            background-color: whitesmoke;
+            border: grey solid 1px;
+            border-radius: 5px;
+        }
+        /* BUTTONS STYLES */
+        .btn {			
+            border: 1px solid lightgray;
+            background-color: darkgreen;
+            color: white;
+            border-radius: 5px;
+            margin-top: 5px;
+            padding: 5px;
+            cursor: pointer;
+            transition: 0.25s;
+            font-size: 12px;
+            text-shadow: #000 0 1px;
+            font-weight: 800;
+        }
+        .btn:hover {
+            background-color: black;
+        }
+        .btn:active {
+            background-color: grey;
+            text-shadow: dimgray 0 2px;
+        }
+        .delCol {
+            float: right;
+            background-color: darkred;
+        }
+        </style>`;
+    }
+}
+
+// COLUMN CLASS
+class ToDoColumn extends ToDoElement {
+    constructor() {
+        super();
+    }
+    set Column(Column) {
+        this.root.innerHTML += `
+        <style>
+            .column {
+                position: relative;
+                padding-top: 10px;
+                padding-bottom: 5px;
+                min-height: 50px;
+                background-color: #bebebe;
+                border-radius: 5px;
+            }
+            .columnHeader {
+                padding: 5px 0;
+                font-size: 24px;
+                cursor: pointer;
+            }
+        </style>
+        <div class="cell">
+            <div class="columnHeader" contenteditable="true">${Column.title}</div>		
+            <div class="column" id="${Column.id}">
+                <span></span>			
+            </div>
+            <button class="btn addCard" id="newCard"><b>+</b> new card</button>
+            <button class="btn delCol"><b>x</b> delete column</button>
+        </div>`;
+    }
+    connectedCallback() {
+        console.log('ToDoColumn connected');
+    }
+}
+
+// CARD CLASS
+class ToDoCard extends ToDoElement {
+    constructor() {
+        super();
+    }
+    set Card(Card) {
+        this.root.innerHTML += `
+        <style>		
+        .card {
+            position: relative;
+            height: 100%;
+            margin: 0px 5px 5px 5px;
+            background-color: #ffffff;
+            border-radius: 5px;
+            cursor: pointer;
+        }	
+        .card .title {
+            padding: 5px;
+            color: black;
+            background-color: #ececec;
+            border-top-left-radius: 5px;
+            border-top-right-radius: 5px;
+            font-size: 1em;
+            font-weight: 600;
+        }
+        .card .description {
+            margin: 0 0 5px 0;
+            padding: 5px;
+            font-size: 0.8em;
+            color: black;
+        }		
+        .card .deleteBtn {
+            float: right;
+            font-size: 1em;
+            color: darkred;
+            font-weight: 600;
+        }
+        </style>
+        <div class="card" id="${Card.id}">
+            <div class="deleteBtn">x&nbsp;&nbsp;</div>
+            <div class="title" contenteditable="true">
+                ${Card.title}			
+            </div>			
+            <div class="description" contenteditable="true">
+                ${Card.description}
+            </div>
+        </div>`;
+    }
+    connectedCallback() {
+        console.log('ToDoCard connected');
+    }
+}
+
+customElements.define('todo-column', ToDoColumn);
+customElements.define('todo-card', ToDoCard);
